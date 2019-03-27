b5b56e4f5a216057873ace2c8eb1612c594f1630
Your Name <you@example.com>
2019-03-27 15:16:28 +0100

    Updates

---
+++ precache-manifest.bf75ba6e58e4bd720b4da0e621082b74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d2764a2b55b6b257a76",
+    "url": "/todo/static/css/main.405ebe41.chunk.css"
+  },
+  {
+    "revision": "1d2764a2b55b6b257a76",
+    "url": "/todo/static/js/main.1c7330fd.chunk.js"
+  },
+  {
+    "revision": "3ce2d46cb5510c372f4d",
+    "url": "/todo/static/js/runtime~main.7a2dc5ec.js"
+  },
+  {
+    "revision": "7fefe0906655373c6029",
+    "url": "/todo/static/js/2.3a169a58.chunk.js"
+  },
+  {
+    "revision": "28d77f8ef8d74c7eeb7ac9bd7408747c",
+    "url": "/todo/index.html"
+  }
+];
